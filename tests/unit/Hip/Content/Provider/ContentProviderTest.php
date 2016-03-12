300a8fa78a6bf7f583d7379d35d4c886a89c7217
Matthew Hansen <inbox@mattyhansen.com>
2016-03-12 16:16:13 +1100

    testing + doctrine migration

---
+++ tests/unit/Hip/Content/Provider/ContentProviderTest.php
@@ -0,0 +1,91 @@
+<?php
+
+class ContentProviderTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+    }
+
+    protected function tearDown()
+    {
+    }
+
+    // tests
+    public function testCanConstruct()
+    {
+        $repo = $this->getMockRepository();
+        $provider = $this->getProvider($repo);
+        static::assertInstanceOf(
+            '\Hip\Content\Provider\ContentProvider',
+            $provider
+        );
+    }
+
+    public function testCanGetWithValidId()
+    {
+        $data = ['anything'];
+
+        $repo = $this->getMockRepository();
+        $repo->expects(static::once())
+            ->method('find')
+            ->will(static::returnValue($data));
+        $provider = $this->getProvider($repo);
+
+        static::assertEquals(
+            $data,
+            $provider->get(1)
+        );
+    }
+
+    public function testCantGetWithInvalidId()
+    {
+        $repo = $this->getMockRepository();
+        $repo->expects(static::once())
+            ->method('find')
+            ->will(static::returnValue(null));
+        $provider = $this->getProvider($repo);
+
+        static::assertNull($provider->get(100000));
+    }
+
+    public function testCanGetAll()
+    {
+        $data = [1,2];
+
+        $repo = $this->getMockRepository();
+        $repo->expects(static::once())
+            ->method('findBy')
+            ->will(static::returnValue($data));
+
+        $provider = $this->getProvider($repo);
+        static::assertEquals(
+            $data,
+            $provider->all(1, 1)
+        );
+    }
+
+    /**
+     * @return PHPUnit_Framework_MockObject_MockObject
+     */
+    private function getMockRepository()
+    {
+        $repo = $this->getMockBuilder('Hip\Content\Repository\ContentRepository')
+            ->disableOriginalConstructor()
+            ->getMock();
+        return $repo;
+    }
+
+    /**
+     * @param $repo
+     * @return \Hip\Content\Provider\ContentProvider
+     */
+    private function getProvider($repo)
+    {
+        $handler = $this->getMockBuilder('Hip\AppBundle\Form\Handler\FormHandler')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $provider = new \Hip\Content\Provider\ContentProvider($repo, $handler);
+        return $provider;
+    }
+}
